098b1246e507e1b8f8ab818a98310ffa7090a68c
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2020-11-17 18:32:56 -0500

    add psuedocode

---
+++ script.js
@@ -0,0 +1,9 @@
+var apiKey = "3c68c7cd73a72f5d17105adff05e1f92";
+
+// get a city input for the desired city
+// make ajax call to  open weather api
+// get desired weather information in the call for the location
+// display the current information in the main card
+// temp, wind speed, humidity, uv index
+
+// display the five day forcast
